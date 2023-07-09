88da81eeef88b8b7cdb137c0257a6bca7a6be9d1
Javlon Alimov <javlonalimov1@gmail.com>
2023-07-09 21:08:13 +0500

    program is working with while

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int maxNumber, maxAnswer = 0;
+            int repetition = 1, maxRepetition = 0;
+            Console.WriteLine("Eng kop takrorlangan sonni topish oyini");
+            Console.Write("Son kiriting :");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            while (inputNumber != 0) 
+            {
+                maxNumber = inputNumber;
+                Console.Write("Son kiriting :");
+                inputNumber = int.Parse(Console.ReadLine());
+
+                if (inputNumber == maxNumber)
+                {
+                    repetition++;
+                }
+                else
+                {
+                    if (repetition >= maxRepetition)
+                    {
+                        maxRepetition = repetition;
+                        maxAnswer = maxNumber;
+                    }
+                    maxNumber = inputNumber;
+                    repetition = 1;
+                }
+            }
+            Console.WriteLine($"Kiritilgan sonlar ichida eng ko'p takrorlangan son " +
+                $" {maxAnswer} soni va u {maxRepetition} marta takrorlandi!");
+            
         }
     }
 }
